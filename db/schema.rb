46f235c4fe93926c111423c2c55ac773a9dd0ae2
Joseph Ruiz <ruizj3@uw.edu>
2020-04-23 01:07:01 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,11 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20160119215120) do
 
-  create_table "classroom_students", force: :cascade do |t|
-    t.integer  "classroom_id"
-    t.integer  "student_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.string   "hometown"
+    t.datetime "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "classrooms", force: :cascade do |t|
@@ -27,12 +27,11 @@ ActiveRecord::Schema.define(version: 20160119215120) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "students", force: :cascade do |t|
-    t.string   "name"
-    t.string   "hometown"
-    t.datetime "birthday"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "classroom_students", force: :cascade do |t|
+    t.integer  "classroom_id"
+    t.integer  "student_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
